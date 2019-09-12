3cb9918035628288f43a23d717f0217724ba9a11
impara <impara1@gmail.com>
2019-09-12 14:24:41 +0200

    Changed Bestil button text

--- js/inline-place2book.js
+++ js/inline-place2book.js
@@ -6,7 +6,7 @@
 jQuery(document).ready(function($) {
 	$('.inline-place2book-ticketinfo').each(function() { 
 	    var obj = this;
-            $(obj).parent().append("<div class='inline-p2b_preloader'><a href='https://place2book.com/da/14540/list/EM3892'>Bestil billet(ter)</a><i class='fa fa-cog fa-spin fa fa-fw'></i><div>");
+            $(obj).parent().append("<div class='inline-p2b_preloader'><a href='https://place2book.com/da/14540/list/EM3892'>Henter billetstatus – gå evt. til arrangementsoversigt hos billetudbyder</a><i class='fa fa-cog fa-spin fa fa-fw'></i><div>");
 		$.getJSON(Drupal.settings.basePath + 'ding/place2book/ticketinfo/' + this.value + "?time=" + new Date().getTime(), function(data) {
 		  $(obj).replaceWith(data['markup']);
                   $('.inline-p2b_preloader').remove();
